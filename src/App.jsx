74cfcfbd2473d0ec7714a3d9783b77677fb1f18b
mejora79 <admin@mejorainfotech.com>
2024-05-08 19:20:31 +0530

    8 May

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Header from "./components/Header/Header.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/tabButton.jsx";
 
 function App() {
+  let tabContent = "Please Click a Button";
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+  }
   return (
     <div>
       <Header />
@@ -23,6 +29,27 @@ function App() {
                 para={CORE_CONCEPTS[3].para}
               />
             </ul>
+          </div>
+        </section>
+
+        <section className="full_width tab cmn_pad">
+          <div className="container">
+            <div className="text_controller">
+              <h2>Example</h2>
+            </div>
+            <menu>
+              <TabButton onSelect={() => handleSelect("Components")}>
+                Components
+              </TabButton>
+              <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect("Props")}>
+                Props
+              </TabButton>
+              <TabButton onSelect={() => handleSelect("State")}>
+                State
+              </TabButton>
+            </menu>
+            <p>{tabContent}</p>
           </div>
         </section>
       </main>
